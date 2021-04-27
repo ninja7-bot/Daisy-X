2cea636963f61faa86796b6ca0146a50acc74e10
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2021-04-27 19:53:43 +0530

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -220,7 +220,6 @@ def start(update: Update, context: CallbackContext):
                  parse_mode=ParseMode.MARKDOWN,
                  disable_web_page_preview=True,
                  reply_markup=InlineKeyboardMarkup(buttons),
-                 parse_mode=ParseMode.MARKDOWN,
                  timeout=60,
              )
     else:
